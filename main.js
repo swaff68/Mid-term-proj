bb7d83daad1ef52ef9f0894080cc942b33efbaab
Swaff <swaff68@gmail.com>
2014-07-11 18:36:18 -0600

    style almost done

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+var supplyArray = [];
+
+var SupplyInput = function (name, weight, qty, howOften){
+
+	this.name = name
+	this.weight = weight
+	this.qty = qty
+	this.howOften = howOften
+};
+
+
+
+
+
+
 $(document).on('ready', function() {
   
 });
